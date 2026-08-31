1909503594c271e53444302f13890dc08d03cbd7
Talibdeen Nasheed <tnasheed77@gmail.com>
2026-08-31 01:05:01 -0400

    Update manifest, add service worker, and resize logo

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'ninga-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
