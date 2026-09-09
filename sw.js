dad0ceca2a5004895dfbc0e97a022e5d9f739a3e
Talibdeen Nasheed <tnasheed77@gmail.com>
2026-09-09 18:16:09 -0400

    Revert converter and return to original BPM and key detector

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ninga-cache-v2';
+const CACHE_NAME = 'ninga-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
@@ -10,21 +10,6 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-// Added activate event to clear old caches so returning users see the new converter update
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
 
